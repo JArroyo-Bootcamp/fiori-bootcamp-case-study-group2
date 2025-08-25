6f3a966c5a4a0d7b238045d83fdb6f139cc31013
chloiemquitco <chloie.m.quitco@accenture.com>
2025-08-25 14:57:04 +0000

    detailpage v1 - linked main colListItem to detail, cancel nav working, productcounter & totalPriceFormatter added

---
+++ webapp/controller/DetailPage.controller.js
@@ -0,0 +1,31 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "casestudygroup2/model/formatter"
+], (Controller, Formatter) => {
+    "use strict";
+
+    return Controller.extend("casestudygroup2.controller.DetailPage", {
+        formatter: Formatter,
+
+        onInit() {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.getRoute("RouteDetailPage").attachPatternMatched(this._onObjectMatched, this);
+        },
+
+        _onObjectMatched: function (oEvent) {
+            var sOrderId = oEvent.getParameter("arguments").OrderID;
+
+            this.getView().bindElement({
+                path: "/Orders(" + sOrderId + ")",
+                parameters: {
+                    expand: "OrderItems, RecPlants, DelPlants"
+                }
+            });
+        },
+
+        onPressDetailCancel: function () {
+            var oRouter = this.getOwnerComponent().getRouter();
+            oRouter.navTo("RouteOrderMainPage");
+        }
+    });
+});
