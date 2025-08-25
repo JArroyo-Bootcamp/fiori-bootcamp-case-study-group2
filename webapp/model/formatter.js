aa33f2d315d68dcdbae845cc81eb0a966268823e
Jerome Arroyo <jerome.arroyo@bms.com>
2025-08-25 10:48:47 +0000

    Order Main Page V1.2

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -15,9 +15,33 @@ sap.ui.define([
 
             }
 
-         }
+         },
 
+          dateFormatter: function (sValue) {
+            if (!sValue) {
+            return "";
+            }
+            let oDate;
 
+            // Handle OData v2 format: /Date(1063016544000)/
+            if (typeof sValue === "string" && sValue.indexOf("/Date(") === 0) {
+                let iTime = parseInt(sValue.replace(/[^0-9]/g, ""), 10);
+                oDate = new Date(iTime);
+            } else {
+                oDate = sValue instanceof Date ? sValue : new Date(sValue);
+            }
+
+            if (isNaN(oDate.getTime())) {
+            return sValue; // fallback if invalid
+            }
+
+            // Format as "01 Jan 2025"
+            let oDateFormat = sap.ui.core.format.DateFormat.getDateInstance({
+                pattern: "dd MMM yyyy"
+            });
+
+            return oDateFormat.format(oDate);
+        }
 
     };
 });
