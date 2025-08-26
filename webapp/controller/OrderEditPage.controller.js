04fcf245051683ca1825c408cacf991eb5cc4f40
Jason Patigayon <jason.patigayon@accenture.com>
2025-08-26 12:48:20 +0000

    Order Edit Page modifications v2

--- webapp/controller/OrderEditPage.controller.js
+++ webapp/controller/OrderEditPage.controller.js
@@ -9,50 +9,47 @@ sap.ui.define([
     return Controller.extend("casestudygroup2.controller.OrderEditPage", {
         formatter: Formatter,
         onInit: function () {
-            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             oRouter.getRoute("RouteEditPage").attachPatternMatched(this._onObjectMatched, this);
         },
 
         _onObjectMatched: function (oEvent) {
-            var sOrderID = oEvent.getParameter("arguments").OrderID;
-            this._sOrderID = sOrderID; // store for later use
-
-            // bind view to the selected order
+            //bind the selected order
+            let sOrderID = oEvent.getParameter("arguments").OrderID;
+            this._sOrderID = sOrderID; 
             this.getView().bindElement("/Orders('" + sOrderID + "')");
-
-            // reference to model
             this._oModel = this.getView().getModel();
         },
 
         onSavePress: function () {
-            var oView = this.getView();
-            var sOrderNumber = oView.byId("inpOrderNumberE").getValue();
-            var sNewStatus = oView.byId("statusMCBEdit").getSelectedKey();
-            var oModel = this._oModel;
-            var sOrderID = this._sOrderID;
-
-            // Show confirmation before saving
+            let oView = this.getView();
+            let sOrderNumber = oView.byId("inpOrderNumberE").getValue();
+            let sNewStatus = oView.byId("statusMCBEdit").getSelectedKey();
+            let oModel = this._oModel;
+            let sOrderID = this._sOrderID;
+            
+            //save confirmation
             MessageBox.confirm(
                 "Are you sure you want to Save these changes?", {
                     actions: [MessageBox.Action.YES, MessageBox.Action.NO],
                     onClose: function (oAction) {
                         if (oAction === MessageBox.Action.YES) {
                             
-                            var oData = {
+                            let oData = {
                                 Status: sNewStatus
                             };
 
                             // update
                             if (oModel.update) {
-                                var sPath = "/Orders('" + sOrderID + "')";
+                                let sPath = "/Orders('" + sOrderID + "')";
                                 oModel.update(sPath, oData, {
                                     success: function () {
-                                        // Show success message
+                                        // show success message
                                         MessageBox.success(
                                             "The Order " + sOrderNumber + " has been successfully updated.", {
                                                 onClose: function () {
-                                                    // Navigate back to detail page
-                                                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                                                    // navigate back to detail page
+                                                    let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                                                     oRouter.navTo("RouteDetailPage", {
                                                         OrderID: sOrderID
                                                     });
@@ -67,14 +64,14 @@ sap.ui.define([
                             }
                             //  update 
                             else {
-                                var oContext = oView.getBindingContext();
+                                let oContext = oView.getBindingContext();
                                 oContext.getObject().Status = sNewStatus;
                                 oModel.refresh(true);
 
                                 MessageBox.success(
                                     "The Order " + sOrderNumber + " has been successfully updated.", {
                                         onClose: function () {
-                                            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                                            let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
                                             oRouter.navTo("RouteDetailPage", {
                                                 OrderID: sOrderID
                                             });
@@ -90,63 +87,67 @@ sap.ui.define([
         },
 
         onCancelPress: function () {
-            MessageBox.confirm(
-                "Are you sure you want to cancel the changes done in the page?", {
-                    actions: [MessageBox.Action.YES, MessageBox.Action.NO],
-                    onClose: function (oAction) {
-                        if (oAction === MessageBox.Action.YES) {
-                            var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-                            oRouter.navTo("RouteDetailPage", {
-                                OrderID: this._sOrderID
-                            });
-                        }
-                    }.bind(this)
-                }
-            );
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let sMessage = oBundle.getText("cancelConfirmationMessage");
+            //for cancel message
+            MessageBox.confirm(sMessage, {
+                actions: [MessageBox.Action.YES, MessageBox.Action.NO],
+                onClose: function (oAction) {
+                    if (oAction === MessageBox.Action.YES) {
+                        let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                        oRouter.navTo("RouteDetailPage", {
+                            OrderID: this._sOrderID
+                        });
+                    }
+                }.bind(this)
+            });
         },
 
         onDeleteProduct: function () {
-            var oTable = this.getView().byId("tblProductEdit");
-            var aSelectedItems = oTable.getSelectedItems();
-
-            if (aSelectedItems.length === 0) {
-                sap.m.MessageToast.show("Please select at least one product to delete.");
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
+            let oTable = this.getView().byId("tblProductEdit");
+            let aSelectedItems = oTable.getSelectedItems();
+            //for delete message
+            if (aSelectedItems.length === 0) {               
+                sap.m.MessageToast.show(oBundle.getText("productDeleteSelectMessage"));
                 return;
             }
-
+            let sConfirmMessage = oBundle.getText("productDeleteConfirmMessage", [aSelectedItems.length]);
+            
             sap.m.MessageBox.confirm(
-                "Are you sure you want to delete " + aSelectedItems.length + " item(s)?", {
+                sConfirmMessage, {
                     actions: [sap.m.MessageBox.Action.YES, sap.m.MessageBox.Action.NO],
                     onClose: function (oAction) {
                         if (oAction === sap.m.MessageBox.Action.YES) {
-                            var oModel = this._oModel;
+                            let oModel = this._oModel;
 
                             aSelectedItems.forEach(function (oItem) {
-                                var oContext = oItem.getBindingContext();
-                                var oProduct = oContext.getObject();
+                                let oContext = oItem.getBindingContext();
+                                let oProduct = oContext.getObject();
 
                                 if (oModel.remove) {
-                                    var sPath = oContext.getPath();
+                                    let sPath = oContext.getPath();
                                     oModel.remove(sPath, {
-                                        success: function () {
-                                            sap.m.MessageToast.show("Product deleted successfully.");
+                                        success: function () {                                           
+                                            sap.m.MessageToast.show(oBundle.getText("productDeleteSuccessMessage"));
                                             oModel.refresh();
                                         },
                                         error: function () {
-                                            sap.m.MessageToast.show("Failed to delete product.");
+                                            
+                                            sap.m.MessageToast.show(oBundle.getText("productDeleteErrorMessage"));
                                         }
                                     });
                                 } else {
-                                    var aData = oModel.getProperty("/OrderItems");
-                                    var iIndex = aData.findIndex(function (prod) {
-                                        return prod.ProductID === oProduct.ProductID;
+                                    let aData = oModel.getProperty("/OrderItems");
+                                    let iIndex = aData.findIndex(function (prod) {
+                                        return prod.ProductName === oProduct.ProductName;
                                     });
                                     if (iIndex !== -1) {
                                         aData.splice(iIndex, 1);
                                         oModel.setProperty("/OrderItems", aData);
                                     }
                                     oModel.refresh(true);
-                                    sap.m.MessageToast.show("Product deleted successfully.");
+                                    sap.m.MessageToast.show(oBundle.getText("productDeleteSuccessMessage"));
                                 }
                             });
 
@@ -158,8 +159,8 @@ sap.ui.define([
         },
 
         onAddProduct: function () {
-            var oView = this.getView();
-            var oModel = this._oModel;
+            let oView = this.getView();
+            let oModel = this._oModel;
 
             if (!this._oProductDialog) {
                 this._oProductDialog = sap.ui.xmlfragment(
@@ -171,17 +172,19 @@ sap.ui.define([
             // open the dialog for product
             this._oProductDialog.open();
 
-            // Get Delivering Plant from current order
-            var sDelPlantID = oView.getBindingContext().getProperty("DelPlantID");
+            // get delivering plant from current order
+            let sDelPlantID = oView.getBindingContext().getProperty("DelPlantID");
 
-            // Filter products
-            var aProducts = oModel.getProperty("/OrderItems") || [];
+            // filter products
+            let aProducts = oModel.getProperty("/OrderItems") || [];
             // show all
-            var aFiltered = aProducts.filter(function (p) {
+            let aFiltered = aProducts.filter(function (p) {
                 return !sDelPlantID || p.DelPlantID === sDelPlantID;
             });
-
-            var oList = this._oProductDialog.byId("productList");
+            
+            // show lists
+            let oList = this._oProductDialog.byId("productList");
+            let oBundle = this.getView().getModel("i18n").getResourceBundle();
             oList.setModel(new sap.ui.model.json.JSONModel({ Products: aFiltered }));
             oList.bindItems({
                 path: "/OrderItems",
@@ -192,60 +195,12 @@ sap.ui.define([
                 })
             });
 
-            // Clear previous search
+            // clear previous search
             oList.getBinding("items").filter([]);
         },
 
-        onProductSearch: function (oEvent) {
-            var sQuery = oEvent.getParameter("query");
-            var oList = this._oProductDialog.byId("productList");
-            var oBinding = oList.getBinding("items");
-
-            if (sQuery) {
-                var aFilters = [
-                    new sap.ui.model.Filter("ProductName", sap.ui.model.FilterOperator.Contains, sQuery),
-                    new sap.ui.model.Filter("Quantity", sap.ui.model.FilterOperator.Contains, sQuery)
-                ];
-            
-                var oCombinedFilter = new sap.ui.model.Filter({
-                    filters: aFilters,
-                    and: false
-                });
-                oBinding.filter([oCombinedFilter]);
-            } else {
-                oBinding.filter([]);
-            }
-        },
-
-        onConfirmProductSelection: function () {
-            var oList = this._oProductDialog.byId("productList");
-            var oSelectedItem = oList.getSelectedItem();
-
-            if (!oSelectedItem) {
-                sap.m.MessageToast.show("Please select a product first.");
-                return;
-            }
-
-            var oProduct = oSelectedItem.getBindingContext().getObject();
-
-            
-            var oNewItem = Object.assign({}, oProduct);
-
-            oNewItem.ItemID = Date.now();  
-            oNewItem.Quantity = 1;         
-            oNewItem.TotalPrice = oNewItem.PricePerQty * oNewItem.Quantity;
-
-            // Push into OrderItems model
-            var aOrderItems = this._oModel.getProperty("/OrderItems") || [];
-            aOrderItems.push(oNewItem);
-            this._oModel.setProperty("/OrderItems", aOrderItems);
-            this._oModel.refresh(true);
-
-            sap.m.MessageToast.show(oProduct.ProductName + " added to order.");
-
-            this._oProductDialog.close();
-        },
         onProductDialogClose: function () {
+            // for click close
             if (this._oProductDialog) {
                 this._oProductDialog.close();
             }
